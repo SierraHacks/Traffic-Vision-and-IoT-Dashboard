42c1d0210b9f0f9ed48fc26de6571345f9283fe0
SierraHacks <ss1967@pleasantonusd.net>
2026-07-22 14:43:45 -0700

    added data storage handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models.post import UserPost
 
 app = FastAPI(
     title="Traffic Vision & IoT Dashboard API",
@@ -8,3 +9,19 @@ app = FastAPI(
 
 traffic_data = []
 sensor_data = []
+
+class TrafficData(BaseModel):
+    camera_id: str
+    average_vehicle = float
+    average_pedestrians: float
+    max_vehicles = str
+    
+@app.post("/traffic")
+def receive_traffic(data: TrafficData):
+
+    traffic_data.append(data)
+
+    return {
+        "message": "Traffic data received",
+        "camera": data.camera_id
+    }
